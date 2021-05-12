665916d8e4e380a8a2af8a57ee496ad59bda4fba
Mayara Leme <mayara.amleme@gmail.com>
2021-05-12 20:13:08 -0300

    Realizando teste de atualização da branch Mayara

---
+++ demo/src/main/java/darasquad/com/demo/DaraSquadApplication.java
@@ -0,0 +1,14 @@
+package darasquad.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaraSquadApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DaraSquadApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/darasquad/com/demo/DaraSquadApplicationTests.java
@@ -0,0 +1,13 @@
+package darasquad.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaraSquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
