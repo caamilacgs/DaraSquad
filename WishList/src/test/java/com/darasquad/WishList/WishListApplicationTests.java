935878ed994e9c7e3df603870b874aebe7adfa45
Mayara Leme <mayara.amleme@gmail.com>
2021-05-15 00:00:16 -0300

    Subindo a pasta de aplicação do spring boot

---
+++ WishList/src/test/java/com/darasquad/WishList/WishListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darasquad.WishList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
